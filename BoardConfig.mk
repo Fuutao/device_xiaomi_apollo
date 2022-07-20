07e7bbcc994c06fc85c990d83ca8e191e52eba32
Alexander Koskovich <akoskovich@pm.me>
2022-07-20 09:16:53 -0700

    apollo: Move BT profiles props to vendor
     * Makes it GSI compatible
    
    Change-Id: I8f7caf241d508af59c16482e6185c42f23f8a460

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -140,7 +140,6 @@ TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
-TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
