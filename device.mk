26b8dabb24f642c73d5ea5103ee5fddbd4bee049
2869145032 <2869145032@qq.com>
2023-01-03 14:11:32 +0800

    apollon: Drop CutoutRingServiceApollo

--- device.mk
+++ device.mk
@@ -138,10 +138,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
-# Camera Cutout
-PRODUCT_PACKAGES += \
-    CutoutRingServiceApollo
-
 # Configstore
 PRODUCT_PACKAGES += \
     disable_configstore
